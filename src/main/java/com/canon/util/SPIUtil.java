c6159c09c84b7323561bdb67e3861995cb2dc45b
zhanzif <zhanzif@worldunion.com.cn>
2019-10-08 14:06:28 +0800

    SPI testDemo

--- src/main/java/com/canon/util/SPIUtil.java
+++ src/main/java/com/canon/util/SPIUtil.java
@@ -1,7 +1,6 @@
 package com.canon.util;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ServiceLoader;
@@ -17,15 +16,28 @@ public abstract class SPIUtil {
 
     public static<T> List<T> init(Class<T> clazz) {
         ServiceLoader<T> load = ServiceLoader.load(clazz);
+        return getImplList(load);
+    }
+
+    public static<T> List<T> init(T t) {
+        return (List<T>) init(t.getClass());
+    }
+
+    public static<T> List<T> init(Class<T> clazz, ClassLoader loader) {
+        ServiceLoader<T> load = ServiceLoader.load(clazz, loader);
+        return getImplList(load);
+    }
+
+    public static<T> List<T> init(T t, ClassLoader loader) {
+        return (List<T>) init(t.getClass(), loader);
+    }
+
+    static<T> List<T> getImplList(ServiceLoader<T> load) {
         List<T> impls = new ArrayList<T>();
         Iterator<T> iterator = load.iterator();
         while (iterator.hasNext()) {
             impls.add(iterator.next());
         }
         return impls;
-    }
-
-    public static<T> List<T> init(T t) {
-        return (List<T>) init(t.getClass());
     }
 }
